ce9e5e7d1d41cacf5e480a658bd76b5ba69492ad
igsnow <zzy716810918@gmail.com>
2019-08-12 15:03:54 +0800

    å页面发送消息失败

--- src/popup.js
+++ src/popup.js
@@ -1,20 +1,15 @@
 // 控制扩展程序的js，比如点击插件开始按钮
 let bg = chrome.extension.getBackgroundPage();
 $(function () {
-
-
+    let pageUrl = '';
+    bg && bg.getCurrentTabUrl(url => {
+        pageUrl = url
+    });
     $("#btn").click(function () {
-        // 在非1688页面点击插件按钮不会导致插件报错
-        if (bg) {
-            bg.getCurrentTabUrl(url => {
-                alert(JSON.stringify(url))
-            })
-
-            // bg.sendMessageToContentScript({cmd: 'batch', value: '开始1688批量采购！'}, res => {
-            //     console.log('来自vwork content的回复：' + res);
-            // });
-            // // 每次重新批量下单时，清空插件的全局数据
-            // bg.clearData()
-        }
+        bg && bg.sendMessageToContentScript({
+            cmd: 'jump', value: pageUrl
+        }, res => {
+            console.log('来自vmall content的回复：' + res);
+        });
     });
 });
