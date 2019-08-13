d506e4b18f5f84998bdb8e7010bc75e0f45a7f3a
igsnow <zzy716810918@gmail.com>
2019-08-13 09:53:58 +0800

    解决直接赋值无法触发v-model值得问题

--- jump/content_script.js
+++ jump/content_script.js
@@ -1,6 +1,6 @@
 // 共用页面的DOM，但是和页面的js是隔离的
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (location.host == 'localhost:8080') {
+    if (location.host == 'localhost:8080' && location.hash == '#/') {
         if (request.cmd == 'jump') {
             console.log(request.value);
             if (!request.value) return;
@@ -8,7 +8,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
             sendResponse('jump消息已收到！');
         }
     } else {
-        console.log('不是mall页面')
+        console.log('不是mall首页')
     }
 });
 
@@ -16,7 +16,8 @@ function handleCopy(val) {
     let ipt = $('.top-banner input');
     let btn = $('.top-banner .btn');
     if (ipt && ipt[0]) {
-        ipt[0].value = val
+        ipt[0].value = val;
+        ipt[0].dispatchEvent(new Event('input'));
     }
     if (btn && btn[0]) {
         console.log('click');
